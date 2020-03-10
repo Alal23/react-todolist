60ae7fa9cdc28ea21a40f821f58484c6b1b0c533
Alal23 <60518138+Alal23@users.noreply.github.com>
2020-03-10 07:40:45 +0700

    make toggle show todoform

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -10,17 +10,19 @@ const TodoList = () => {
     { text: "Learning React Hooks!" },
     { text: "Learning styling in React!" }
   ]);
+  const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = value => {
     const addedTodo = [...todos, { text: value }];
 
     setTodos(addedTodo);
   };
+  const showAddToggle = () => setShowAdd(!showAdd);
 
   return (
     <Paper>
-      <Header />
-      <TodoForm addTodo={addTodo} />
+      <Header showAddToggle={showAddToggle} />
+      <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} />
     </Paper>
   );
